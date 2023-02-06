82d8246f48ef1b9c577004b43c6b4a3f9f6cfcee
Josué <josuebvrios@gmail.com>
2023-02-06 16:15:34 -0300

    Percorrendo a lista

--- JavaScript/01. JavaScript para Web. Crie páginas dinâmicas/main.js
+++ JavaScript/01. JavaScript para Web. Crie páginas dinâmicas/main.js
@@ -71,8 +71,10 @@ function tocaSomPom () {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
 /*
+listaDeTeclas[0].onclick = tocaSomPom;
 listaDeTeclas[1].onclick = tocaSomClap;
 listaDeTeclas[2].onclick = tocaSomTim;
 listaDeTeclas[3].onclick = tocaSomPuff;
@@ -83,3 +85,10 @@ listaDeTeclas[7].onclick = tocaSomTic;
 listaDeTeclas[8].onclick = tocaSomTom;
 */
 
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
