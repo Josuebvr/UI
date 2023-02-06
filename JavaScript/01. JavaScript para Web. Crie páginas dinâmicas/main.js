681b811c365720638756a451fd778359ccb836f0
Josué <josuebvrios@gmail.com>
2023-02-06 11:42:17 -0300

    Lista de elementos

--- JavaScript/01. JavaScript para Web. Crie páginas dinâmicas/main.js
+++ JavaScript/01. JavaScript para Web. Crie páginas dinâmicas/main.js
@@ -1,5 +1,64 @@
+/*---------------------------------------------------------------------POM----------------------------------------------------------------------*/
+
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
-
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+/*--------------------------------------------------------------------CLAP----------------------------------------------------------------------*/
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+/*---------------------------------------------------------------------TIM----------------------------------------------------------------------*/
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+/*--------------------------------------------------------------------PUFF----------------------------------------------------------------------*/
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+/*-------------------------------------------------------------------SPLASH---------------------------------------------------------------------*/
+
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+/*--------------------------------------------------------------------TOIM----------------------------------------------------------------------*/
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+/*---------------------------------------------------------------------PSH----------------------------------------------------------------------*/
+
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+/*---------------------------------------------------------------------TIC----------------------------------------------------------------------*/
+
+function tocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+/*---------------------------------------------------------------------TOM----------------------------------------------------------------------*/
+
+function tocaSomTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------*/
